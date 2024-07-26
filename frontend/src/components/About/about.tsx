76983282f4d2d19448ada38cdb58be8cd01ac7ba
sabith <sabithmuhammed136@gmail.com>
2024-07-26 10:15:59 +0530

    footer design change

--- frontend/src/components/About/about.tsx
+++ frontend/src/components/About/about.tsx
@@ -2,6 +2,9 @@
 
 export const About = () => {
   return (
-    <div>about</div>
+    <div className="bg-white h-[100px]
+    ">
+      <div className="w-32 h-11 bg-slate-500 rounded-md" style={{boxShadow: "4px 4px 0px 0px rgba(0,0,0,0.75)"}}></div>
+    </div>
   )
 }
--- frontend/src/components/Footer/footer.tsx
+++ frontend/src/components/Footer/footer.tsx
@@ -7,6 +7,7 @@ import { GrLinkedinOption } from "react-icons/gr";
 export const Footer = () => {
     return (
         <div className="flex flex-col items-center w-dvw  bg-black text-white px-10 py-5">
+            <hr className="bg-gray-800 w-full my-4"/>
             <div className="w-full flex flex-col md:flex-row items-center md:justify-between">
                 <Link href={"/"} className="md:self-start max-md:mb-5">
                     <Image
@@ -21,22 +22,22 @@ export const Footer = () => {
                 <div className="flex flex-col max-md:mb-5">
                     <p>Follow us on</p>
                     <div className="flex mt-3">
-                        <div className="p-1 bg-blue-400 text-black rounded-md me-2">
+                        <div className="p-1 bg-black border-primary border-[2px] text-primary rounded-md me-2 cursor-pointer" style={{boxShadow: "2px 2px 0px 0px #EEE4B1"}}>
                             <IoLogoInstagram size={24} />
                         </div>
-                        <div className="p-1  bg-blue-400 text-black rounded-md mx-2">
+                        <div className="p-1  bg-black border-primary border-[2px] text-primary rounded-md me-2 cursor-pointer" style={{boxShadow: "2px 2px 0px 0px #EEE4B1"}}>
                             <IoLogoGithub size={24} />
                         </div>
-                        <div className="p-1  bg-blue-400 text-black rounded-md mx-2">
+                        <div className="p-1 bg-black border-primary border-[2px] text-primary rounded-md me-2 cursor-pointer" style={{boxShadow: "2px 2px 0px 0px #EEE4B1"}}>
                             <FaUpwork size={24} />
                         </div>
-                        <div className="p-1  bg-blue-400 text-black rounded-md mx-2">
+                        <div className="p-1  bg-black border-primary border-[2px] text-primary rounded-md me-2 cursor-pointer" style={{boxShadow: "2px 2px 0px 0px #EEE4B1"}}>
                             <GrLinkedinOption size={24} />
                         </div>
                     </div>
                 </div>
             </div>
-            <hr className="bg-gray-800 w-full my-4"/>
+            {/* <hr className="bg-gray-800 w-full my-4"/> */}
             <p className="text-center text-sm">
                 Undefined, All rights reserved 2024
             </p>
